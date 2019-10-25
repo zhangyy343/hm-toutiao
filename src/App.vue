566704942b3aba3c88056212e3574e7763a71a7a
zhangyy343 <429110621@qq.com>
2019-10-25 19:44:02 +0800

    背景

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,13 @@
 <template>
   <div id="app">
-
+    <router-view />
   </div>
 </template>
 
 <script>
-
 export default {
   name: 'app'
-
 }
 </script>
 
-<style lang="less">
-
-</style>
+<style lang="less"></style>
---
+++ src/views/login/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container">
+    <el-card>
+      <img src="../../assets/logo_index.png" width="200px" />
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="less">
+.container {
+  width: 100%;
+  height: 100%;
+  background: url(../../assets/login_bg.jpg) no-repeat center / cover;
+  position: absolute;
+  left: 0;
+  top: 0;
+}
+//element组件
+.el-card {
+  width: 400px;
+  height: 370px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+}
+</style>
