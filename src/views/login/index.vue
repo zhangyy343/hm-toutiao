1b68cac5ede4dca3dee18374d2cafbd2ee616a62
zhangyy343 <429110621@qq.com>
2019-10-25 20:51:19 +0800

    message

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -2,12 +2,40 @@
   <div class="container">
     <el-card>
       <img src="../../assets/logo_index.png" width="200px" />
+      <el-form ref="form" :model="loginForm">
+        <el-form-item>
+          <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input
+            v-model="loginForm.code"
+            style="width:235px;margin-right:10px"
+            placeholder="请输入验证码"
+          ></el-input>
+          <el-button>发送验证码</el-button>
+        </el-form-item>
+        <el-form-item>
+          <el-checkbox :value="true">我已阅读并同意用户协议和隐私条款</el-checkbox>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" style="width:100%">立即登录</el-button>
+        </el-form-item>
+      </el-form>
     </el-card>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      loginForm: {
+        mobile: '',
+        code: ''
+      }
+    }
+  }
+}
 </script>
 
 <style lang="less">
@@ -27,6 +55,9 @@ export default {}
   left: 50%;
   top: 50%;
   transform: translate(-50%, -50%);
-  text-align: center;
+  img {
+    display: block;
+    margin: 0 auto 30px;
+  }
 }
 </style>
